051bad754f2eb478c51907a0a986cda4eb747588
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2021-03-26 01:07:48 +0900

    Use Hamcrest to write assertions

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
@@ -1,7 +1,8 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
-import org.junit.Assert.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.junit.Test
 
 class StatisticsUtilsKtTest {
@@ -17,7 +18,7 @@ class StatisticsUtilsKtTest {
         val result = getActiveAndCompletedStats(tasks)
 
         // Check the result
-        assertEquals(result.completedTasksPercent, 0f)
-        assertEquals(result.activeTasksPercent, 100f)
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(100f))
     }
 }
