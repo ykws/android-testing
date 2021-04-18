8a927d8742f344f191067a144ea554a3d0e14a27
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2021-04-18 23:56:24 +0900

    Create FakeTestRepository

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeTestRepository.kt
@@ -0,0 +1,64 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import androidx.lifecycle.LiveData
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+
+class FakeTestRepository : TasksRepository {
+    override suspend fun getTasks(forceUpdate: Boolean): Result<List<Task>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun refreshTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeTasks(): LiveData<Result<List<Task>>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun refreshTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeTask(taskId: String): LiveData<Result<Task>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTask(taskId: String, forceUpdate: Boolean): Result<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun completeTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun completeTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun clearCompletedTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+}
